bcbb3d4b4dc6525f146ba459202f9ac0f685357a
André Meneses <andremeneses@gmail.com>
2019-10-28 10:48:30 +0000

    Procfile

--- videogoals/settings.py
+++ videogoals/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
