9056d78c2a317f3157126c7dd813d3c406dc20c6
André Meneses <andre.meneses@arpa.tech>
2019-11-08 11:36:32 +0000

    added static root

--- videogoals/settings.py
+++ videogoals/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.environ.get('STATIC_ROOT')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
