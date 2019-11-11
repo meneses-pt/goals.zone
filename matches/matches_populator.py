054f90a0e3583774841f5c5ec2f2cab911fc77ae
André Meneses <andre.meneses@arpa.tech>
2019-11-11 09:30:04 +0000

    Update matches_populator.py

--- matches/matches_populator.py
+++ matches/matches_populator.py
@@ -10,34 +10,36 @@ import json
 @background(schedule=60 * 60)
 def fetch_new_matches():
     print('Fetching new matches...')
-    response = _fetch_data_from_api()
-    data = json.loads(response.content)
-    results = data['api']['results']
-    print(f'{results} matches fetched...')
-    for fixture in data['api']['fixtures']:
-        home_team = fixture['homeTeam']['team_name']
-        away_team = fixture['awayTeam']['team_name']
-        home_goals = fixture['goalsHomeTeam']
-        away_goals = fixture['goalsAwayTeam']
-        score = None
-        if home_goals and away_goals:
-            score = f'{home_goals}:{away_goals}'
-        datetime_str = _get_datetime_string(fixture['event_date'])
-        match_datetime = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S%z')
-        print(f'{home_team} - {away_team} | {score} at {match_datetime}')
-        match = Match()
-        match.home_team = home_team
-        match.away_team = away_team
-        match.score = score
-        match.datetime = match_datetime
-        _save_or_update_match(match)
+    start_date = date.today() - timedelta(days=5)
+    for single_date in (start_date + timedelta(n) for n in range(6)):
+        response = _fetch_data_from_api(single_date)
+        data = json.loads(response.content)
+        results = data['api']['results']
+        print(f'{results} matches fetched...')
+        for fixture in data['api']['fixtures']:
+            home_team = fixture['homeTeam']['team_name']
+            away_team = fixture['awayTeam']['team_name']
+            home_goals = fixture['goalsHomeTeam']
+            away_goals = fixture['goalsAwayTeam']
+            score = None
+            if home_goals and away_goals:
+                score = f'{home_goals}:{away_goals}'
+            datetime_str = _get_datetime_string(fixture['event_date'])
+            match_datetime = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S%z')
+            print(f'{home_team} - {away_team} | {score} at {match_datetime}')
+            match = Match()
+            match.home_team = home_team
+            match.away_team = away_team
+            match.score = score
+            match.datetime = match_datetime
+            _save_or_update_match(match)
+        print(f'Ended processing day {single_date}')
     print('Ended processing matches')
     pass
 
 
-def _fetch_data_from_api():
-    today = date.today()
-    today_str = today.strftime("%Y-%m-%d")
+def _fetch_data_from_api(single_date):
+    today_str = single_date.strftime("%Y-%m-%d")
     headers = {
         "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
         "X-RapidAPI-Key": "pcPW7okGUwmshMx1FkHBxaFEHfOYp1d7uahjsnjOy1uaG957NX"
