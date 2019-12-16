ac34704a8dc174afa5afac06a5fae9e6256f36a3
André Meneses <andre.meneses@arpa.tech>
2019-12-16 11:19:43 +0000

    Added Analytics, Removed Scorebat

--- matches/goals_populator.py
+++ matches/goals_populator.py
@@ -12,67 +12,6 @@ from matches.models import Match, VideoGoal
 def fetch_videogoals():
     print('Fetching new goals')
     _fetch_reddit_goals()
-    _fetch_scorebat_goals()
-
-
-def _fetch_scorebat_goals():
-    response = _fetch_data_from_scorebat_api()
-    data = json.loads(response.content)
-    if not isinstance(data, list) or len(data) == 0:
-        print(f'No data in response: {response.content}')
-        return
-    else:
-        results = len(data)
-        print(f'{results} posts fetched...')
-        for entry in data:
-            for video in entry['videos']:
-                video_title = video['title']
-                teams = entry['title']
-                teams_splitted = teams.split(" - ")
-                home = teams_splitted[0]
-                away = teams_splitted[1]
-                minute = re.findall('\s([0-9+]*)\'', video_title)
-                if len(minute) > 0:
-                    minute_str = minute[0].strip()
-                else:
-                    minute_str = ''
-                    print(f'Minute not found for: {video_title}')
-                matches = Match.objects.filter(home_team__name__unaccent__trigram_similar=home,
-                                               away_team__name__unaccent__trigram_similar=away,
-                                               datetime__gte=date.today() - timedelta(days=2))
-
-                url = re.findall('src=\'([^\']*)\'', video['embed'])
-                if len(url) > 0:
-                    url = url[0]
-                else:
-                    print(f'No url found [{home}]-[{away}] for: {video_title}')
-                    continue
-                if matches.exists():
-                    match = matches.first()
-                    # print(f'Match {match} found for: {title}')
-                    try:
-                        videogoal = VideoGoal.objects.get(permalink__exact=url)
-                    except VideoGoal.DoesNotExist:
-                        videogoal = VideoGoal()
-                        videogoal.permalink = url
-                    videogoal.match = match
-                    videogoal.url = url
-                    videogoal.title = video_title
-                    videogoal.minute = minute_str
-                    videogoal.save()
-                    # print('Saved: ' + title)
-                else:
-                    print(f'No match found in database [{home}]-[{away}] for: {video_title}')
-                    pass
-
-
-def _fetch_data_from_scorebat_api():
-    headers = {
-        "User-agent": "Goals Populator 0.1"
-    }
-    response = requests.get(f'https://www.scorebat.com/video-api/v1/', headers=headers)
-    return response
-    pass
 
 
 def _fetch_reddit_goals():
--- videogoals/settings.py
+++ videogoals/settings.py
@@ -118,6 +118,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+INTERNAL_IPS = (
+    '127.0.0.1'
+)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
