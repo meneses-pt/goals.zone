dfb7847f07d2239b2d1b60b5e5d99084de4bf8d9
meneses-pt <andre@meneses.pt>
2022-02-21 16:24:45 +0000

    goals.africa development

--- africa/urls.py
+++ africa/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.AfricaMatchesListView.as_view(), name='index'),
+    path('history', views.AfricaMatchesHistoryListView.as_view(), name='history'),
+    path('teams', views.AfricaTeamsListView.as_view(), name='teams-list'),
+
+    path('<slug:slug>', views.AfricaMatchDetailView.as_view(), name='match-detail'),
 ]
--- africa/views.py
+++ africa/views.py
@@ -1,6 +1,40 @@
+from datetime import date, timedelta, datetime
 from django.views import generic
 
 from matches.models import Match
+from matches.utils import localize_date
+
+
+class AfricaMatchesHistoryListView(generic.ListView):
+    template_name = 'africa/matches/match_history_list.html'
+
+    def get_queryset(self):
+        try:
+            query_date_str = self.request.GET.get('date')
+            query_date_obj = datetime.strptime(query_date_str, '%Y-%m-%d')
+            start_date = query_date_obj
+            end_date = query_date_obj + timedelta(days=1)
+        except (ValueError, TypeError):
+            start_date = datetime.combine(datetime.today(), datetime.min.time())
+            end_date = start_date + timedelta(days=1)
+        start_date = localize_date(start_date)
+        end_date = localize_date(end_date)
+        return Match.objects.order_by('datetime').filter(datetime__gte=start_date,
+                                                         datetime__lt=end_date,
+                                                         videogoal__isnull=False).distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super(AfricaMatchesHistoryListView, self).get_context_data(**kwargs)
+        try:
+            query_date_str = self.request.GET.get('date')
+            query_date_obj = datetime.strptime(query_date_str, '%Y-%m-%d')
+        except (ValueError, TypeError):
+            query_date_obj = datetime.today()
+        context['date'] = query_date_obj
+        if query_date_obj.date() < date.today():
+            context['date_next'] = query_date_obj + timedelta(days=1)
+        context['date_prev'] = query_date_obj - timedelta(days=1)
+        return context
 
 
 class AfricaMatchesListView(generic.ListView):
@@ -11,3 +45,23 @@ class AfricaMatchesListView(generic.ListView):
 
     def get_queryset(self):
         return Match.objects.order_by('-datetime').filter(videogoal__isnull=False).distinct()
+
+
+class AfricaMatchDetailView(generic.DetailView):
+    template_name = 'africa/matches/match_detail.html'
+    model = Match
+
+
+class AfricaTeamsListView(generic.ListView):
+    template_name = 'africa/matches/team_list.html'
+    paginate_by = 25
+
+    def get_queryset(self):
+        return Team.objects.raw('''
+                    select t.id, t.name, t.logo_url, t.logo_file, t.name_code, count(m.id) as matches_count
+                    from matches_team t
+                    inner join matches_match m on t.id = m.home_team_id or t.id = m.away_team_id
+                    inner join matches_videogoal vg on m.id = vg.match_id
+                    group by t.id, name, logo_url, logo_file, name_code
+                    order by matches_count desc
+                ''')
