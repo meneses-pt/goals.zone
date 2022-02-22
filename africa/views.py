b29f64f35aac3b5024d289fd3ebe48a4fe46b17f
meneses-pt <andre@meneses.pt>
2022-02-22 16:41:00 +0000

    Fixed issues with goals.africa

--- africa/views.py
+++ africa/views.py
@@ -96,8 +96,8 @@ class AfricaTeamsDetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(AfricaTeamsDetailView, self).get_context_data(**kwargs)
-        home_matches = self.object.home_team
-        away_matches = self.object.away_team
+        home_matches = self.object.home_team.all()
+        away_matches = self.object.away_team.all()
         team_matches = sorted(chain(home_matches, away_matches), key=lambda instance: instance.datetime, reverse=True)
         paginator = Paginator(team_matches, self.paginate_by)
         page_number = self.request.GET.get('page')
