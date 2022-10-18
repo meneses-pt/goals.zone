8d3f6ad4c91826291ba93a71adadd742b99c6f8d
meneses-pt <andre@meneses.pt>
2022-10-18 14:50:26 +0100

    Hosts protection

--- goals_zone/settings.py
+++ goals_zone/settings.py
@@ -36,8 +36,26 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-ALLOWED_HOSTS = ["goals.zone", "videogoals.meneses.pt", "127.0.0.1", "localhost"]
-CSRF_TRUSTED_ORIGINS = ["https://goals.zone"]
+ALLOWED_HOSTS = ["goals.zone", "goals.africa", ".meneses.pt", "127.0.0.1", "localhost"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://goals.zone",
+    "https://goals.africa",
+    "https://*.meneses.pt",
+    "http://127.0.0.1",
+    "http://localhost",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://goals.zone",
+    "https://goals.africa",
+    "https://gzreact.meneses.pt",
+    "https://gz.meneses.pt",
+    "https://videogoals.meneses.pt",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+]
 
 # Application definition
 
@@ -72,15 +90,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_hosts.middleware.HostsResponseMiddleware",
-]
-
-
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "https://goals.zone",
-    "https://goals.africa",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = "goals_zone.urls"
