60c9aec92aa1505d9c60faa169e7ab8072020455
meneses-pt <andre@meneses.pt>
2022-10-20 10:56:51 +0100

    Disable webpages

--- matches/urls.py
+++ matches/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 
 from . import views
-from .feeds import LatestMatchesFeed, LatestVideosFeed
+
+# from .feeds import LatestMatchesFeed, LatestVideosFeed
 
 urlpatterns = [
-    path("", views.MatchesListView.as_view(), name="index"),
-    path("history", views.MatchesHistoryListView.as_view(), name="history"),
-    path("teams", views.TeamsListView.as_view(), name="teams-list"),
-    path("<slug:slug>", views.MatchDetailView.as_view(), name="match-detail"),
-    path("teams/<slug:slug>", views.TeamsDetailView.as_view(), name="teams-detail"),
-    # api
+    # #PAGES
+    # path("", views.MatchesListView.as_view(), name="index"),
+    # path("history", views.MatchesHistoryListView.as_view(), name="history"),
+    # path("teams", views.TeamsListView.as_view(), name="teams-list"),
+    # path("<slug:slug>", views.MatchDetailView.as_view(), name="match-detail"),
+    # path("teams/<slug:slug>", views.TeamsDetailView.as_view(), name="teams-detail"),
+    # #API
     path("api/matches/", views.MatchesApiListView.as_view(), name="api-matches-list"),
     path("api/matches/<slug:slug>", views.MatchesApiDetailView.as_view(), name="api-match-detail"),
     path("api/matches-search/", views.MatchSearchView.as_view(), name="api-matches-search"),
@@ -20,7 +22,7 @@ urlpatterns = [
     ),
     path("api/teams/", views.TeamSearchView.as_view(), name="api-teams-list"),
     path("api/teams/<slug:slug>", views.TeamApiDetailView.as_view(), name="api-teams-detail"),
-    # rss
-    path("rss/matches", LatestMatchesFeed()),
-    path("rss/videos", LatestVideosFeed()),
+    # #RSS
+    # path("rss/matches", LatestMatchesFeed()),
+    # path("rss/videos", LatestVideosFeed()),
 ]
