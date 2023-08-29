0f58605aa3ad12c0ec5cd46c3afdd4f12113ce59
meneses-pt <andre@meneses.pt>
2023-08-29 10:38:36 +0100

    Remove cloudscraper and capsolver. Added Scrapfly

--- matches/proxy_request.py
+++ matches/proxy_request.py
@@ -1,12 +1,14 @@
 import json
 import random
+from urllib.parse import quote
 
-import cloudscraper
+import requests
 from fake_headers import Headers
 from fp.fp import FreeProxy
+from requests import Response
 
 from goals_zone import settings
-from goals_zone.settings import CAPSOLVER_API_KEY
+from goals_zone.settings import SCRAPFLY_API_KEY
 
 
 class ProxyRequest:
@@ -22,9 +24,6 @@ class ProxyRequest:
             ProxyRequest.__instance__ = self
         else:
             raise Exception("You cannot create another ProxyRequest class")
-        self.scraper = cloudscraper.create_scraper(
-            captcha={"provider": "capsolver", "api_key": CAPSOLVER_API_KEY}
-        )
 
     @staticmethod
     def get_instance():
@@ -41,32 +40,59 @@ class ProxyRequest:
         response = None
         attempts = 0
         while (response is None or response.status_code != 200) and attempts < max_attempts:
-            # Make half of the attempts with the PREMIUM_PROXY
-            if (
-                attempts < (max_attempts / 2)
-                and settings.PREMIUM_PROXY
-                and settings.PREMIUM_PROXY != ""
-            ):
-                ports_list = [12322, 12323, 22323]
-                # Ports from 11200 to 11250
-                for i in range(11200, 11251):
-                    ports_list.append(i)
-                proxy = settings.PREMIUM_PROXY[:-5] + str(random.choice(ports_list))
-                self.current_proxy = proxy
-            else:
-                self.current_proxy = FreeProxy(rand=True).get()
+            # Make one third of the attempts with each strategy
+            if use_proxy:
+                if attempts < (max_attempts / 3):
+                    self.current_proxy = None  # Scrapfly
+                elif (
+                    attempts < (max_attempts * 2 / 3)
+                    and settings.PREMIUM_PROXY
+                    and settings.PREMIUM_PROXY != ""
+                ):
+                    ports_list = [12322, 12323, 22323]
+                    # Ports from 11200 to 11250
+                    for i in range(11200, 11251):
+                        ports_list.append(i)
+                    proxy = settings.PREMIUM_PROXY[:-5] + str(random.choice(ports_list))
+                    self.current_proxy = proxy
+                else:
+                    self.current_proxy = FreeProxy(rand=True).get()
+
+                # If scrapfly is wating too many credits pass it to second
+                # if (
+                #     attempts < (max_attempts / 3)
+                #     and settings.PREMIUM_PROXY
+                #     and settings.PREMIUM_PROXY != ""
+                # ):
+                #     ports_list = [12322, 12323, 22323]
+                #     # Ports from 11200 to 11250
+                #     for i in range(11200, 11251):
+                #         ports_list.append(i)
+                #     proxy = settings.PREMIUM_PROXY[:-5] + str(random.choice(ports_list))
+                #     self.current_proxy = proxy
+                # elif attempts < (max_attempts * 2 / 3):
+                #     self.current_proxy = None  # Scrapfly
+                # else:
+                #     self.current_proxy = FreeProxy(rand=True).get()
             try:
-                print(f"Proxy {self.current_proxy} | Attempt {attempts + 1}", flush=True)
+                print(
+                    f"Proxy {'Scrapfly' if self.current_proxy is None else self.current_proxy} | "
+                    f"Attempt {attempts + 1}",
+                    flush=True,
+                )
                 attempts += 1
                 if use_proxy:
-                    response = self.scraper.get(
-                        url,
-                        proxies={"https": f"http://{self.current_proxy}"},
-                        headers=headers,
-                        timeout=timeout,
-                    )
+                    if self.current_proxy is None:  # Scrapfly
+                        response = make_scrapfly_request(url, headers, timeout)
+                    else:
+                        response = requests.get(
+                            url,
+                            proxies={"https": f"http://{self.current_proxy}"},
+                            headers=headers,
+                            timeout=timeout,
+                        )
                 else:
-                    response = self.scraper.get(url, headers=headers, timeout=timeout)
+                    response = requests.get(url, headers=headers, timeout=timeout)
                 if response.status_code != 200:
                     raise Exception(
                         "Wrong Status Code: "
@@ -91,3 +117,38 @@ class ProxyRequest:
             )
             raise Exception("Number of attempts exceeded trying to make request: " + str(url))
         return response
+
+
+def make_scrapfly_request(url, headers, timeout):
+    original_url = quote(url, safe="")
+    headers_str = ""
+    for headers_key in headers:
+        headers_str += (
+            f"&headers[{quote(headers_key, safe='')}]={quote(headers[headers_key], safe='')}"
+        )
+    url = (
+        f"https://api.scrapfly.io/scrape"
+        f"?key={SCRAPFLY_API_KEY}"
+        f"&asp=true"
+        f"&url={original_url}"
+    )
+    url += headers_str
+    response = requests.get(url, headers=headers, timeout=timeout)
+    if response.status_code != 200 or response.json()["result"]["status_code"] != 200:
+        raise Exception(
+            "Wrong Status Code: "
+            + str(response.json()["result"]["status_code"])
+            + "|"
+            + str(response.json()["result"]["content"])
+            + "---"
+            + str(response.status_code)
+            + "|"
+            + str(response.content)
+        )
+    resp = Response()
+    resp_result = response.json()["result"]
+    str_content = resp_result["content"]
+    str_encoding = resp_result["content_encoding"]
+    resp._content = str_content.encode(str_encoding)
+    resp.status_code = resp_result["status_code"]
+    return resp
